edec4e65ba602b759334c87807611bd8bb460410
Kyle B. Johnson <kbjohnson90@gmail.com>
2014-08-19 12:14:52 -0400

    Create adf.php

---
+++ application/config/adf.php
@@ -0,0 +1,16 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| ADF XML Library Configuration (Auto-Lead Data Format)
+|--------------------------------------------------------------------------
+|
+| specify default information for the adf xml specification
+*/
+
+$config['dealer_name'] = '';
+$config['crm_email'] = array();
+$config['from_email'] = '';
+
+/* End of file adf.php */
+/* Location: ./application/config/adf.php */
