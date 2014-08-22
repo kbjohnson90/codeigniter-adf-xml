b9d1204b21f738901256282b666ec523faa24233
Kyle B. Johnson <kbjohnson90@gmail.com>
2014-08-22 16:20:37 -0400

    Update adf.php

--- application/libraries/adf.php
+++ application/libraries/adf.php
@@ -7,37 +7,39 @@
 *
 * @package codeigniter-adf-xml
 * @author Kyle B. Johnson (http://www.kylebjohnson.me)
-* @version 0.1.0
+* @version 0.2.0
 * @license The MIT License Copyright (c) 2014 Kyle B. Johnson
 */
 class Adf {
 
-	public $requestdate;
-	public $vehicle;
-	public $customer;
-	public $vendor;
-	public $provider;
+	public $adf;
 
-	function __construct() {
+	function __construct()
+	{
 		$this->ci =& get_instance();
 		$this->ci->load->config('adf', TRUE);
 
-		$this->requestdate = date("c");
+		$this->adf = new SimpleXMLElement('<?adf version="1.0"?><adf/>');
+		$this->prospect = $this->adf->addChild('prospect');
 
-		$this->vendor = $this->ci->config->item('dealer_name', 'adf');
+		$this->requestdate = $this->prospect->addChild('requestdate', date("c"));
+
+		$vendor = $this->ci->config->item('vendor_name', 'adf');
+		$this->vendor = $this->prospect->addChild('vendor')->addChild('vendorname', $vendor);
+
+		$provider = $this->ci->config->item('provider_name', 'adf');
+		$this->provider = $this->prospect->addChild('provider')->addChild('name', $provider);
 	}
 
-	public function init($customer, $vehicle = null, $provider = null) {
-		$this->customer = $customer;
-		if(isset($vehicle)) $this->vehicle = $vehicle;
-		if(isset($provider)) $this->provider = $provider;
-	}
-
-	public function send() {
+	/**
+	 * Send ADF XML to CRM
+	 */
+	public function send()
+	{
 	    $to = $this->ci->config->item('crm_email', 'adf');
 	    if (is_array($to)) $to = implode(',', $to);
 
-	    $subject = $this->provider; //Promotion Title
+	    $subject = $this->provider->__toString(); //Promotion Title
 
 	    $headers  = 'From: ' . $this->ci->config->item('from_email', 'adf') . "\r\n";
 	    $headers .= "xml version: 1.0" . "\r\n";
@@ -46,94 +48,121 @@ class Adf {
 		return mail($to, $subject, $this->xml(), $headers);
 	}
 
+	/**
+	 * Output ADF XML
+	 */
 	public function xml()
 	{
-        $adf = new SimpleXMLElement('<?adf version="1.0"?><adf/>');
+	    return $this->adf->saveXML();
+	}
 
-        $prospect = $adf->addChild('prospect');
+	/**
+	 * Add a vehicle to the prospect
+	 */
+	public function vehicle($data, $interest = null, $status = null, $comments = null)
+	{
+		$this->vehicle = $this->prospect->addChild('vehicle');
+		
+		foreach ($data as $key => $value) {
+			$this->vehicle->addChild($key, $value);
+		}
 
-        //Request Date
-        $requestdate = $prospect->addChild('requestdate', $this->requestdate);
+		if (isset($interest)) $this->vehicle->addAttribute('interest', $interest);
+		if (isset($status)) $this->vehicle->addAttribute('status', $status);
+	}
 
-        //Vehicle
-        if (isset($this->vehicle)) {
-        	$vehicle = $prospect->addChild('vehicle');
+	/**
+	 * Add a customer to the prospect
+	 */
+	public function customer($name, $comments = null)
+	{
+		$this->customer = $this->prospect->addChild('customer');
+		$this->contact = $this->customer->addChild('contact');
 
-        	if (isset($this->vehicle['year'])) $year = $vehicle->addChild('year', $this->vehicle['year']);
-        	if (isset($this->vehicle['make'])) $year = $vehicle->addChild('make', $this->vehicle['make']);
-        	if (isset($this->vehicle['model'])) $year = $vehicle->addChild('model', $this->vehicle['model']);
-        	if (isset($this->vehicle['comments'])) $year = $vehicle->addChild('comments', $this->vehicle['comments']);
-        }
+		if (is_array($name))
+		{
+			foreach ($name as $part => $value) {
+				$name = $this->contact->addChild('name', $value);
+				$name->addAttribute('part', $part);
+			}
+		}
+		else
+		{
+			$this->contact->addChild('name', $name);
+			$name->addAttribute('part', 'full');
+		}
 
-        //Customer
-        if (isset($this->customer)) {
-        	$customer = $prospect->addChild('customer');
-        	
-        	if (isset($this->customer['contact'])) {
+		if (isset($comments)) $this->customer->addChild('comments', $comments);
 
-				$contact = $customer->addChild('contact');
+		return $this;
+	}
 
-				//Name
-				if (isset($this->customer['contact']['name'])) {
+	/**
+	 * Add an email address to the customer contact
+	 */
+	public function email($email)
+	{
+		$this->contact->addChild('email', $email);
 
-					if (!is_array($this->customer['contact']['name'])) {
-						$name = $contact->addChild('name', $this->customer['contact']['name']);
-						$name->addAttribute('part', 'full');
-					} else {
-						foreach ($this->customer['contact']['name'] as $key => $value) {
-							$name[$key] = $contact->addChild('name', $value);
-							if (is_string($key))  $name[$key]->addAttribute('part', $key);
-						}
-					}
+		return $this;
+	}
+
+	/**
+	 * Add a phone number to the customer contact
+	 */
+	public function phone($number, $type = null, $time = null, $besttime = false)
+	{
+		$number = $this->contact->addChild('phone', $number);
+
+		if (isset($type)) $number->addAttribute('type', $type);
+		if (isset($time)) $number->addAttribute('time', $time);
+		if ($besttime) $number->addAttribute('besttime', true);
+
+		return $this;
+	}
+
+	/**
+	 * Add an address to the customer contact
+	 */
+	public function address($data, $type = null)
+	{
+		$address = $this->contact->addChild('address');
+
+		foreach ($data as $key => $value)
+		{
+			if (is_array($value) && $key == 'street')
+			{
+				foreach ($value as $line => $text)
+				{
+					$address->addChild('street', $text)
+							->addAttribute('line', $line + 1);
 				}
-
-        		//Phone
-        		if (isset($this->customer['contact']['phone'])) {
-
-					if (!is_array($this->customer['contact']['phone'])) {
-						$phone = $contact->addChild('phone', $this->customer['contact']['phone']);
-					} else {
-						foreach ($this->customer['contact']['phone'] as $key => $value) {
-							$phone[$key] = $contact->addChild('phone', $value);
-							$phone[$key]->addAttribute('type', 'voice'); //TODO: find a way to specify non-voice numbers
-							if (is_string($key)) $phone[$key]->addAttribute('time', $key);
-						}
-					}
-        		}
-
-        		//Email
-        		if (isset($this->customer['contact']['email'])) {
-        			$email = $contact->addChild('email', $this->customer['contact']['email']);
-        		}
-
-        		//Address
-        		if (isset($this->customer['contact']['address'])) {
-        			$address = $contact->addChild('address');
-
-        			foreach ($this->customer['contact']['address'] as $key => $value) {
-        				$$key = $address->addChild($key, $value);
-        			}
-        		}
-        	}
-
-        	if (isset($this->customer['comments'])) $comments = $customer->addChild('comments', $this->customer['comments']);
-        }
-
-		//Vendor
-		if (isset($this->vendor)) {
-			$vendor  = $prospect->addChild('vendor');
-			$contact = $vendor->addChild('contact');
-			$contact->addChild('name', $this->vendor);
+			}
+			else
+			{
+				$address->addChild($key, $value);
+			}
 		}
 
-		//Provider
-		if (isset($this->provider)) {
-			$provider = $prospect->addChild('provider');
-			$contact = $provider->addChild('contact');
-			$contact->addChild('name', $this->provider);
-		}
+		if (isset($type)) $address->addAttribute('type', $type);
 
-	    return $adf->saveXML();
+		return $this;
+	}
+
+	/**
+	 * Add a vendor to the prospect
+	 */
+	public function vendor($name)
+	{
+		$this->vendor = $this->prospect->addChild('vendor')->addChild('name', $name);
+	}
+
+	/**
+	 * Add a provider to the prospect
+	 */
+	public function provider($name)
+	{
+		$this->provider = $this->prospect->addChild('provider')->addChild('name', $name);
 	}
 
 }
